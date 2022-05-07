1ea5f69db1bd94459543c473f0fe33d8b9419942
Roberto García <roberto@rhizomik.net>
2022-05-07 14:00:41 +0200

    Script to unzip inputs and use YOLOv5

---
+++ detect-inputs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+jq -r '.[]' <(echo $DIDS) | while read i; do
+
+  unzip -j /data/inputs/$i/0 -d /data/input/ #UNZIP INPUT DATA to /data/input/
+
+done
+
+wait
+
+python detect.py --weights /model/best.pt --source /data/input/ # run yolov5 python to detect images at /data/input using trained model best.pt
+
+wait
+
+cd /usr/src/app/runs/detect/ # Change workdir
+
+wait
+
+zip -r results.zip exp*/* # Zip the results
+
+wait
+
+mv results.zip /data/outputs/. # Move results to /data/outputs/. to get them uploaded
+
+wait
